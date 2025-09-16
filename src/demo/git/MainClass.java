a680d9c5f80c4ed06897719ae0cc51dc4a779496
DEEPAN-R <deepan41103@gmail.com>
2025-09-16 18:36:34 +0530

    Revert "First commit in deepanOne"
    This reverts commit aef7e25aec461f27ee1bccc8be2e3c0d0d82d154.

--- src/demo/git/MainClass.java
+++ src/demo/git/MainClass.java
@@ -4,10 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(calculate(10, 11));
-	}
-	public static int calculate(int a,int b) {
-		return a +b;
+
 	}
 
 }
